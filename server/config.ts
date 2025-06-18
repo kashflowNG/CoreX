d8072aa2c08428392a0a0f19679d227b9d4691ec
NerochazeDev <nerochazedev@yahoo.com>
2025-06-18 12:20:04 +0100

    Update config.ts
    Update database

--- server/config.ts
+++ server/config.ts
@@ -3,7 +3,7 @@
 
 export const config = {
   // Database configuration
-  DATABASE_URL: process.env.DATABASE_URL || "postgresql://neondb_owner:npg_6Axcj9JzErpt@ep-tiny-surf-a5txedpl.us-east-2.aws.neon.tech/neondb?sslmode=require",
+  DATABASE_URL: process.env.DATABASE_URL || "postgresql://corex_database_user:bvwmeRXL3en1lw1i9NYnqxuXHJCtuU5V@dpg-d199jq15pdvs73e430o0-a/corex_database",
   
   // Session configuration
   SESSION_SECRET: process.env.SESSION_SECRET || "5fC7gluqorDXJBsk11WetGFDd7s1ec47VezC3fRNhFRDAU5Yx2OA6US3kYtZx+/VCCuDYZoufk+050B3SopuCw==",
